f1583ab18811ab881ad9bddd35f5bd3ce9e1534d
احمد تحسين <54220580+ahmed-tahseen@users.noreply.github.com>
2019-11-01 22:35:38 +0300

    AT-Kali
    hello 
    welcome kali linux && kali nethunter
    
    git clone github.com/ahmed-tahseen/AT-Kali
    
    python AT-Kali

---
+++ AT-Kali.py
@@ -0,0 +1,24 @@
+v = "\033[0;32m"
+print (v+"     /       \_ ")
+print (v+"    / |  4 4  | \ ")
+print (v+"   /  |__   __|  \ ")
+print (v+"   |__/((o| |o))\__| ")
+print (v+"   |      | |      | ")
+print (v+"   |\     |_|     /| ")
+print (v+"   | \           / | ")
+print (v+"   \| /  ___  \ |/ ")
+print (v+"    \ | / _ \ | / ")
+print (v+"     \_________/ ")
+print (v+"       _|_____|_ ")
+print (v+"   ___|_________|____ ")
+
+
+print ("By Ahmed Tahseen")
+
+print ("Tool AT-Kali")
+
+print ("Email- ahmedjsql7@gmail.com")
+
+import os
+os.system ("apt install kali-linux-all")
+   
